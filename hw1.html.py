54b895ecfe36ba695553782629c374ad882fdb75
youjin08302 <youjin08302@naver.com>
2021-03-10 17:24:44 +0900

    Create hw1.html.py

---
+++ hw1.html.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. r=0.5, f<sub>0</sub>=0.5
+
+# In[10]:
+
+
+n=0
+r=0.5
+s=0.5
+
+while (n<10) :
+      s=r*s*(1-s)
+      n=n+1
+      print(n,s)
+
+
+# 2. r=2.5, f<sub>0</sub>=0.5
+
+# In[11]:
+
+
+n=0
+r=2.5
+s=0.5
+
+while (n<10) :
+      s=r*s*(1-s)
+      n=n+1
+      print(n,s)
+
+
+# 3. r=4.5, f<sub>0</sub>=0.5
+
+# In[12]:
+
+
+n=0
+r=4.5
+s=0.5
+
+while (n<10) :
+      s=r*s*(1-s)
+      n=n+1
+      print(n,s)
+
+
+# 4. r=4.5, f<sub>0</sub>=0.51
+
+# In[13]:
+
+
+n=0
+r=4.5
+s=0.51
+
+while (n<10) :
+      s=r*s*(1-s)
+      n=n+1
+      print(n,s)
+
